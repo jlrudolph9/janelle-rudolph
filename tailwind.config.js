a303b1c303c86495f2553d8d76a2fe8db497809b
jlrudolph9 <jlrudolph9@gmail.com>
2024-07-19 10:44:50 -0700

    more tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   purge: ['./src/**/*.html', './src/**/*.js'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: media,
   theme: {
     extend: {},
   },
